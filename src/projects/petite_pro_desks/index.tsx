fa5f4765014390510f16d5dc850e0868e68b48de
lance13c <pianoman13c@gmail.com>
2020-05-17 14:41:50 -0400

    home banner styling, adds colors

--- src/projects/petite_pro_desks/index.tsx
+++ src/projects/petite_pro_desks/index.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
+import './styles/index.scss';
 
 export interface PetiteProDeskProps {}
 
 const PetiteProDesk: React.SFC<PetiteProDeskProps> = (props) => {
 	return (
-		<section>
+		<section id="ppd-app">
 			<nav />
-			<main>
-				<h1>
-					A Petite Professional Personalizable
-					<span>Desk</span>
-					<h2>The desk made smaller indivisuals.</h2>
-				</h1>
+			<div className="main-placeholder" />
+			<main className="main">
+				<div className="content-wrapper">
+					<h1 className="title is-1">Desks for Petite Professionals.</h1>
+					<h2 className="subtitle is-3 flex justify-start items-center flex-wrap">
+						<span>Small</span>
+						<i className="fas fa-circle is-size-8 p-5" />
+						<span>Ergonomic</span>
+						<i className="fas fa-circle is-size-8 p-5" />
+						<span>Hand Crafted</span>
+					</h2>
+				</div>
 			</main>
-			<section>
-				<h1>Attributes</h1>
+			<section className="section bg-linear-alpha-gray-100">
+				<h1 className="title is-2">Attributes</h1>
 				<ul className="grid">
 					<li />
 					<li />
@@ -24,15 +31,15 @@ const PetiteProDesk: React.SFC<PetiteProDeskProps> = (props) => {
 					<li />
 				</ul>
 			</section>
-			<section>
-				<h1>Our Mission</h1>
+			<section className="section bg-purple-100">
+				<h1 className="title is-2">Our Mission</h1>
 				<p className="special-text">
 					Mission Statement Here adlkfjadlkfjalkdfjalkdfjlakdjflakdfjlakdjflakdjfl kdjfal dkfjal kdfjaldk
 					jfdalk jdflk jadlfkjadlkfj
 				</p>
 			</section>
-			<section>
-				<h1>Stay Informed</h1>
+			<section className="section bg-pink-200">
+				<h1 className="title is-2">Stay Informed</h1>
 				<form action="">
 					<div className="row">
 						<input type="text" name="first-name" />
